3b892fffde033e6470c21d0cc44aec253f39289a
Brian Chan <i@b123400.net>
2019-02-23 20:11:50 +0900

    Implement prescription model

---
+++ src/main/java/com/ywesee/amiko/Prescription.java
@@ -0,0 +1,52 @@
+package com.ywesee.amiko;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Prescription {
+    public static final String KEY_AMK_HASH = "prescription_hash";
+    public static final String KEY_AMK_PLACE_DATE = "place_date";
+    public static final String KEY_AMK_PATIENT = "patient";
+    public static final String KEY_AMK_OPERATOR = "operator";
+    public static final String KEY_AMK_MEDICATIONS = "medications";
+
+    public String hash;
+    public String placeDate;
+    public Operator doctor;
+    public Patient patient;
+    public ArrayList<Product> medications;
+
+    public Prescription(JSONObject json) {
+        this.hash = json.optString(KEY_AMK_HASH);
+        this.placeDate = json.optString(KEY_AMK_PLACE_DATE);
+        try {
+            this.patient = new Patient(json.getJSONObject(KEY_AMK_PATIENT));
+            this.doctor = new Operator(json.getJSONObject(KEY_AMK_OPERATOR));
+        } catch (Exception e) {
+            Log.w("Amiko.Prescription", e.toString());
+        }
+    }
+
+    public JSONObject toJSON() {
+        JSONObject j = new JSONObject();
+        try {
+            j.put(KEY_AMK_HASH, this.hash);
+            j.put(KEY_AMK_PLACE_DATE, this.placeDate);
+            j.put(KEY_AMK_PATIENT, this.patient.toJSON());
+            j.put(KEY_AMK_OPERATOR, this.doctor.toJSON());
+
+            JSONArray medis = new JSONArray();
+            for (Product p : this.medications) {
+                medis.put(p.toJSON());
+            }
+            j.put(KEY_AMK_MEDICATIONS, medis);
+        } catch (Exception e) {
+            Log.w("Amiko.Prescription", e.toString());
+        }
+        return j;
+    }
+}
