bcb6a4e5cf6ac2297c4d1b56475748a05ff0aa2c
Brian Chan <i@b123400.net>
2019-04-08 03:55:03 +0900

    Draw products and page number in PDF

--- src/main/java/com/ywesee/amiko/PrescriptionPrintingUtility.java
+++ src/main/java/com/ywesee/amiko/PrescriptionPrintingUtility.java
@@ -12,6 +12,7 @@ import android.print.pdf.PrintedPdfDocument;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
+import android.util.Size;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -31,16 +32,14 @@ public class PrescriptionPrintingUtility {
     }
     private static float a4Width = mm2pix(210);
     private static float a4Height = mm2pix(297);
+    private static float marginX = 50;
 
     static final int MILS_PER_INCH = 1000;
     static final int POINTS_IN_INCH = 72;
 
-    public static void generatePDF(Context context, Prescription prescription) {
+    public static File generatePDF(Context context, Prescription prescription, String filename) {
         float margin = mm2pix(18);
 //        float fontSize = 11.0;
-
-        float medY = mm2pix(110);
-        float medSpacing = mm2pix(8);
 
         PrintAttributes.MediaSize a4 = PrintAttributes.MediaSize.ISO_A4;
         PrintAttributes printAttrs = new PrintAttributes.Builder()
@@ -55,40 +54,52 @@ public class PrescriptionPrintingUtility {
         int pageNumber = 1;
         float originY = 0;
         boolean needToDrawHeader = true;
+        float medY = mm2pix(110);
+        float medSpacing = mm2pix(8);
+
         PdfDocument.Page page = document.startPage(1);
         Canvas canvas = page.getCanvas();
         if (prescription.medications.size() == 0) {
             drawHeader(originY, canvas, prescription);
         }
-        for (Product p : prescription.medications) {
+
+        for (int i = 0; i < prescription.medications.size(); i++) {
+            Product p = prescription.medications.get(i);
             if (needToDrawHeader) {
+                drawPageNumber(context, canvas, pageNumber);
                 drawHeader(originY, canvas, prescription);
                 needToDrawHeader = false;
+                originY = medY;
             }
-            if (originY > a4Height - margin) {
-                // next page
+            originY = drawProduct(p, canvas, originY, a4Height - margin - originY);
+            if (originY < 0) {
+                originY = 0;
+                needToDrawHeader = true;
+                pageNumber++;
+                document.finishPage(page);
+                page = document.startPage(pageNumber);
+                canvas = page.getCanvas();
+                i--;
+            } else {
+                originY += medSpacing;
             }
         }
-        // do final processing of the page
+        // do final processing of the last page
         document.finishPage(page);
-        // Here you could add more pages in a longer doc app, but you'd have
-        // to handle page-breaking yourself in e.g., write your own word processor...
-        // Now write the PDF document to a file; it actually needs to be a file
-        // since the Share mechanism can't accept a byte[]. though it can
-        // accept a String/CharSequence. Meh.
         try {
-            File f = new File(context.getFilesDir() + "/test.pdf");
+            PrescriptionUtility.ensureDirectory(PrescriptionUtility.pdfDirectory(context));
+            File f = new File(PrescriptionUtility.pdfDirectory(context), filename);
             FileOutputStream fos = new FileOutputStream(f);
             document.writeTo(fos);
             document.close();
             fos.close();
+            return f;
         } catch (IOException e) {
             throw new RuntimeException("Error generating file", e);
         }
     }
 
-    public static void drawHeader(float originY, Canvas canvas, Prescription prescription) {
-        float marginX = 50;
+    public static float drawHeader(float originY, Canvas canvas, Prescription prescription) {
         float fontSize = 11;
 //        float filenameY = mm2pix(50);
         float docY = mm2pix(60);
@@ -100,15 +111,15 @@ public class PrescriptionPrintingUtility {
         paint.setColor(Color.BLACK);
         paint.setTextSize(fontSize);
 
-        Rect doctorRect = drawMultipleLinesTextWithRightTop(doctorString, canvas, paint, a4Width - marginX, originY + docY);
+        Size doctorRect = drawMultipleLinesTextWithRightTop(doctorString, canvas, paint, a4Width - marginX, originY + docY);
 
         String patientString = prescription.patient.getStringForPrescriptionPrinting();
         drawMultipleLinesText(patientString, canvas, paint, marginX, originY + patY);
 
         Bitmap signature = prescription.doctor.getSignatureThumbnailForPrinting();
         if (signature != null) {
-            int signatureX = (int) (a4Width - doctorRect.width() - marginX);
-            int signatureY = (int) (originY + docY + doctorRect.height());
+            int signatureX = (int) (a4Width - marginX - doctorRect.getWidth());
+            int signatureY = (int) (originY + docY + doctorRect.getHeight());
             canvas.drawBitmap(
                     signature,
                     new Rect(0, 0, signature.getWidth(), signature.getHeight()),
@@ -117,12 +128,56 @@ public class PrescriptionPrintingUtility {
             );
         }
 
-        drawMultipleLinesText(prescription.placeDate, canvas, paint, marginX, originY + placeDateY);
+        Size placeDateRect = drawMultipleLinesText(prescription.placeDate, canvas, paint, marginX, originY + placeDateY);
+        return originY + placeDateY + placeDateRect.getHeight();
     }
 
-    private static Rect drawMultipleLinesTextWithRightTop(String str, Canvas canvas, TextPaint paint, float xOfRight, float top) {
+    private static float drawProduct(Product product, Canvas canvas, float originY, float availableHeight) {
+        // If not enough space -> return -1;
+        float fontSize = 11;
+        TextPaint paint = new TextPaint();
+        paint.setTextSize(fontSize);
+
+        Size packageInfoSize = sizeOfMultipleLinesText(product.packageInfo, paint);
+        Size eanSize = sizeOfMultipleLinesText(product.eanCode, paint);
+        Size commentSize = new Size(0, 0);
+        if (product.comment != null && !product.comment.equals("")) {
+            commentSize = sizeOfMultipleLinesText(product.comment, paint);
+        }
+
+        if (packageInfoSize.getHeight() + eanSize.getHeight() + commentSize.getHeight() > availableHeight) {
+            return -1;
+        }
+
+        paint.setColor(Color.rgb(11, 36, 251));
+        drawMultipleLinesText(product.packageInfo, canvas, paint, marginX, originY);
+        originY += packageInfoSize.getHeight();
+
+        paint.setColor(Color.rgb(128, 128, 128));
+        drawMultipleLinesText(product.eanCode, canvas, paint, marginX, originY);
+        originY += eanSize.getHeight();
+
+        if (commentSize.getHeight() > 0) {
+            paint.setColor(Color.rgb(128, 128, 128));
+            drawMultipleLinesText(product.comment, canvas, paint, marginX, originY);
+            originY += commentSize.getHeight();
+        }
+
+        return originY;
+    }
+
+    private static void drawPageNumber(Context context, Canvas canvas, int pageNumber) {
+        TextPaint paint = new TextPaint();
+        float fontSize = 11;
+        paint.setTextSize(fontSize);
+        float pageNumberY = mm2pix(50);
+        String pageString = context.getString(R.string.page) + " " + Integer.toString(pageNumber);
+        drawMultipleLinesTextWithRightTop(pageString, canvas, paint, a4Width - marginX, pageNumberY);
+    }
+
+    private static Size drawMultipleLinesTextWithRightTop(String str, Canvas canvas, TextPaint paint, float xOfRight, float top) {
         StaticLayout textLayout = new StaticLayout(
-                str, paint, (int)a4Width, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+                str, paint, (int)(a4Width - marginX * 2), Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
 
         float stringWidth = 0;
         for (int i = 0; i < textLayout.getLineCount(); i++) {
@@ -134,20 +189,43 @@ public class PrescriptionPrintingUtility {
         textLayout.draw(canvas);
         canvas.restore();
 
-        return new Rect(
-                0,
-                0,
+        return new Size(
                 (int)stringWidth,
                 textLayout.getHeight()
         );
     }
 
-    private static void drawMultipleLinesText(String str, Canvas canvas, TextPaint paint, float left, float top) {
+    private static Size drawMultipleLinesText(String str, Canvas canvas, TextPaint paint, float left, float top) {
         StaticLayout textLayout = new StaticLayout(
-                str, paint, (int)a4Width, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+                str, paint, (int)(a4Width - marginX * 2), Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+
+        float stringWidth = 0;
+        for (int i = 0; i < textLayout.getLineCount(); i++) {
+            stringWidth  = max(stringWidth , textLayout.getLineMax(i));
+        }
+
         canvas.save();
         canvas.translate(left, top);
         textLayout.draw(canvas);
         canvas.restore();
+
+        return new Size(
+                (int)stringWidth,
+                textLayout.getHeight()
+        );
+    }
+
+    private static Size sizeOfMultipleLinesText(String str, TextPaint paint) {
+        StaticLayout textLayout = new StaticLayout(
+                str, paint, (int)(a4Width - marginX * 2), Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+
+        float stringWidth = 0;
+        for (int i = 0; i < textLayout.getLineCount(); i++) {
+            stringWidth  = max(stringWidth , textLayout.getLineMax(i));
+        }
+        return new Size(
+                (int)stringWidth,
+                textLayout.getHeight()
+        );
     }
 }
