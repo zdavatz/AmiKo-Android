99a2f9721c36976f3136bdc3a31eee79b9f92506
b123400 Chan <i@b123400.net>
2019-12-25 01:44:00 +0900

    Use json for saving favourite #164

--- src/main/java/com/ywesee/amiko/FavoriteStore.java
+++ src/main/java/com/ywesee/amiko/FavoriteStore.java
@@ -19,12 +19,17 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 package com.ywesee.amiko;
 
+import android.util.JsonReader;
+import android.util.JsonWriter;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
 
 public class FavoriteStore {
@@ -69,33 +74,59 @@ public class FavoriteStore {
 
 	public HashSet<String> load(String path) {
 		HashSet<String> hs = new HashSet<String>();
+		boolean canReadJson = false;
 		try {
-			FileInputStream file_in = new FileInputStream(m_dir + "/" + path);
-			// Make sure there is something to read...
-			if (file_in.available()>0) {
-				ObjectInputStream in = new ObjectInputStream(file_in);
-				hs = (HashSet<String>)in.readObject();
-				in.close();
+			FileInputStream inputStream = new FileInputStream(m_dir + "/" + path);
+			JsonReader reader = new JsonReader(new InputStreamReader(inputStream, "UTF-8"));
+			reader.beginArray();
+			while (reader.hasNext()) {
+				hs.add(reader.nextString());
 			}
-			file_in.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+			reader.endArray();
+			canReadJson = true;
+		} catch (Exception e) {
+			canReadJson = false;
+		}
+
+		if (!canReadJson) {
+			// Fallback to the old format
+			try {
+				FileInputStream file_in = new FileInputStream(m_dir + "/" + path);
+				// Make sure there is something to read...
+				if (file_in.available()>0) {
+					ObjectInputStream in = new ObjectInputStream(file_in);
+					hs = (HashSet<String>)in.readObject();
+					in.close();
+				}
+				file_in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
 		}
 		return hs;
 	}
 
 	public void save(String path, HashSet<String> hs) {
+		FileOutputStream stream = null;
 		try {
-			FileOutputStream file_out = new FileOutputStream(m_dir + "/" + path);
-			ObjectOutputStream out = new ObjectOutputStream(file_out);
-			out.writeObject(hs);
-			out.close();
-			file_out.close();
-			// System.out.println("Serialized data is saved in " + m_dir);
+			stream = new FileOutputStream(m_dir + "/" + path);
+			JsonWriter writer = new JsonWriter(new OutputStreamWriter(stream, "UTF-8"));
+
+			writer.beginArray();
+			for (String str : hs) {
+				writer.value(str);
+			}
+			writer.endArray();
+			writer.close();
+			stream.close();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				stream.close();
+			} catch (Exception e) { }
 		}
 	}
 }
