604a97dacbc184d9739905889abb16d5269f146a
b123400 <i@b123400.net>
2023-09-19 22:41:42 +0900

    Class for ADSwiss auth handle

---
+++ src/main/java/com/ywesee/amiko/hinclient/ADSwissAuthHandle.java
@@ -0,0 +1,38 @@
+package com.ywesee.amiko.hinclient;
+
+import com.ywesee.amiko.Utilities;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+
+public class ADSwissAuthHandle {
+    public String token;
+    public Date lastUsedAt;
+    public Date expiresAt;
+
+    public ADSwissAuthHandle(String token) {
+        this.token = token;
+        this.lastUsedAt = new Date();
+        this.expiresAt = new Date(new Date().getTime() + (12 * 60 * 60) * 1000); // 12 hours
+    }
+
+    public ADSwissAuthHandle(JSONObject json) throws JSONException {
+        this.token = json.getString("token");
+        this.lastUsedAt = Utilities.dateFromTimeString(json.getString("last_used_at"));
+        this.expiresAt = Utilities.dateFromTimeString(json.getString("expires_at"));
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("token", this.token);
+        json.put("last_used_at", Utilities.timeStringFromDate(this.lastUsedAt));
+        json.put("expires_at", Utilities.timeStringFromDate(this.expiresAt));
+        return json;
+    }
+
+    public boolean isExpired() {
+        return this.expiresAt.before(new Date()) || new Date(this.lastUsedAt.getTime() + 60 * 60 * 2 * 1000 /*2 hours*/).before(new Date());
+    }
+}
